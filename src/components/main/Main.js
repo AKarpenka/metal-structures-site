61ab73457a2777c5a9ad1bddee31274e5769f5e6
Andrei Klimiankou <AKlimiankou@gomel.iba.by>
2021-02-11 12:43:28 +0300

    init commit

---
+++ src/components/main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Main.scss';
+
+class Main extends React.Component {
+    render() {
+        
+    }
+}
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/main/Main';
+import './index.scss';
+
+ReactDOM.render(
+    <div>
+        <Main />
+    </div>,
+    document.getElementById('root')
+);
