2c59b37e567de54aa148d234a954996ef1a77fb7
Andrei Klimiankou <AKlimiankou@gomel.iba.by>
2021-02-18 20:30:53 +0300

    header content

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,11 +7,39 @@ class Header extends React.Component {
     render() {
         return (
             <div className="container top-rect">
-                <div className="row justify-content-end">
-                    <div className="text-color-white">
-                        <div>+375 (29) 154-24-63</div>
-                        <div>+375 (25) 154-24-63</div>
-                        <div>Пн-Вс 8.00 - 20.00</div>
+                <div>
+                    <div className="text-right">+375 (29) 154-24-63</div>
+                    <div className="text-right">+375 (25) 154-24-63</div>
+                    <div className="text-right">Пн-Вс 8.00 - 20.00</div>
+                </div>
+                <div className="row">
+                    <div className="col-4">
+                        <div>МЕТАЛЛОКОНСТРУКЦИИ В ГОМЕЛЕ</div>
+                    </div>
+                    <div className="col-8">
+                        <div className="d-flex align-items-center justify-content-between">
+                            <div>Главная</div>
+                            <div>Продукция</div>
+                            <div>О нас</div>
+                            <div>Калькулятор стоимости</div>
+                            <div>Контакты</div>
+                            <div>Заказать звонок</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center text-center">
+                    <div className="col-12">
+                        <h1>Lorem ipsum dolor sit amet</h1>
+                        <div>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                            mollit anim id est laborum.
+                        </div>
+                        <div>ПОДРОБНЕЕ --{'>'}</div>
                     </div>
                 </div>
             </div>
