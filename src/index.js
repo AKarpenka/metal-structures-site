6ca45062b9639f778c30750c71538f164fd03a8a
Andrei Klimiankou <AKlimiankou@gomel.iba.by>
2021-03-26 15:34:03 +0300

    connect router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Route, Switch, Redirect, Router } from 'react-router-dom';
 import Main from './components/main/Main';
+import createBrowserHistory from './utils/history';
 import createReduxStore from './redux';
 import './index.scss';
 
 ReactDOM.render(
     <Provider store={createReduxStore}>
-        <Main />
+        <Router history={createBrowserHistory}>
+            <Switch>
+                <Redirect exact from="/" to="/main" />
+                <Route path="/main" component={Main} />
+            </Switch>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
