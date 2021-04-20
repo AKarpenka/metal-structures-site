c6b6691befa64f91b9291636a8ac6184e4cb9354
Andrei Klimiankou <AKlimiankou@gomel.iba.by>
2021-04-21 00:17:40 +0300

    nodemailer for call modal

--- api/server/index.js
+++ api/server/index.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 const PORT = process.env.PORT || 8888;
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.post('/callMe', (req, res) => {
-    res.json({ message: 'Hello from server!' });
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        auth: {
+            user: 'mail',
+            pass: 'password'
+        }
+    });
+
+    const mailOptions = {
+        from: 'from',
+        to: 'to',
+        subject: 'Test mail',
+        text: `${req.body.message}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.end();
+        } else {
+            console.log(`Email sent: ${info.response}`);
+            res.end();
+        }
+    });
 });
 
 app.listen(PORT, () => {
--- src/components/modals/call/CallModal.js
+++ src/components/modals/call/CallModal.js
@@ -1,41 +1,79 @@
-/* eslint-disable react/no-unused-prop-types */
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { hideCallModal } from '../../../redux/actions/modalAction';
-import api from '../../../api/mails';
+import { askForRecalling } from '../../../redux/actions/requestActions';
 import './CallModal.scss';
 
 export class CallModal extends React.Component {
-    sendMail = userData => {
-        return api.callMe(userData).then(() => this.hideCallModalWindow());
+    constructor(props) {
+        super(props);
+        this.state = { username: '', telephone: '', message: '' };
+    }
+
+    handleUsernameChange = event => {
+        this.setState({ username: event.target.value });
     };
 
-    hideCallModalWindow = () => {
-        this.props.hideCallModal();
+    handleTelephoneChange = event => {
+        this.setState({ telephone: event.target.value });
+    };
+
+    handleMessageChange = event => {
+        this.setState({ message: event.target.value });
+    };
+
+    handleSubmit = event => {
+        event && event.preventDefault();
+        if (!!this.state.username && !!this.state.telephone) {
+            this.props.askForRecalling({
+                username: this.state.username,
+                telephone: this.state.telephone,
+                message: this.state.message
+            });
+        }
     };
 
     render() {
+        const { username, telephone, message } = this.state;
+
         return (
             <div className="call-modal-window text-center">
                 <h3 className="header-modal font-s-18">ЗАКАЗАТЬ ЗВОНОК</h3>
                 <p className="text-modal font-s-14">
                     Введите свои данные и мы перезвоним для уточнения деталей:
                 </p>
-                <div className="fields">
-                    <div>
-                        <input type="text" placeholder="Ваше имя *" required />
+                <form onSubmit={this.handleSubmit}>
+                    <div className="fields">
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Ваше имя *"
+                                value={username}
+                                onChange={this.handleUsernameChange}
+                                required
+                            />
+                        </div>
+                        <div className="field">
+                            <textarea
+                                placeholder="Ваше сообщение"
+                                value={message}
+                                onChange={this.handleMessageChange}
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Ваш телефон *"
+                                value={telephone}
+                                onChange={this.handleTelephoneChange}
+                                required
+                            />
+                        </div>
                     </div>
-                    <div className="field">
-                        <textarea placeholder="Ваше сообщение" />
-                    </div>
-                    <div>
-                        <input type="text" placeholder="Ваш телефон *" required />
-                    </div>
-                </div>
-                <button type="button" className="btn btn-navy" onClick={() => this.sendMail('')}>
-                    ОТПРАВИТЬ
-                </button>
+                    <button type="submit" className="btn btn-navy">
+                        ОТПРАВИТЬ
+                    </button>
+                </form>
                 <p className="footer-modal font-s-11">
                     Нажимая на кнопку, вы даете согласие на обработку своих персональных данных
                 </p>
@@ -45,11 +83,11 @@ export class CallModal extends React.Component {
 }
 
 CallModal.propTypes = {
-    hideCallModal: PropTypes.func
+    askForRecalling: PropTypes.func
 };
 
 const mapDispatchToProps = {
-    hideCallModal
+    askForRecalling
 };
 
 export default connect(null, mapDispatchToProps)(CallModal);
