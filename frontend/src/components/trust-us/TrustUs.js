5b4fef97a40211b7380429ccd4d81ddcd3465a3a
Andrei Klimiankou <AKlimiankou@gomel.iba.by>
2021-04-25 13:58:36 +0300

    carousel

--- frontend/src/components/trust-us/TrustUs.js
+++ frontend/src/components/trust-us/TrustUs.js
@@ -1,9 +1,53 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import Farmaciya from '../../assets/images/clients/farmaciya.png';
+import FavApteka from '../../assets/images/clients/fav_apteka.png';
+import PolyVet from '../../assets/images/clients/poly_vet.png';
+import SelHoz from '../../assets/images/clients/sel_hoz.png';
+import Sugar from '../../assets/images/clients/sugar.png';
 import './TrustUs.scss';
 
 class TrustUs extends React.Component {
     render() {
+        const images = [Farmaciya, FavApteka, PolyVet, SelHoz, Sugar];
+
+        const responsive = {
+            superLargeDesktop: {
+                // the naming can be any, depends on you.
+                breakpoint: { max: 4000, min: 3000 },
+                items: 5
+            },
+            desktop: {
+                breakpoint: { max: 3000, min: 1024 },
+                items: 4
+            },
+            tablet: {
+                breakpoint: { max: 1024, min: 464 },
+                items: 3
+            },
+            mobile: {
+                breakpoint: { max: 464, min: 0 },
+                items: 2
+            }
+        };
+        const CustomLeftArrow = ({ onClick }) => (
+            <div className="arrow-background-left" onClick={() => onClick()}>
+                <i className="custom-left-arrow" />
+            </div>
+        );
+        const CustomRightArrow = ({ onClick }) => (
+            <div className="arrow-background-right" onClick={() => onClick()}>
+                <i className="custom-right-arrow" />
+            </div>
+        );
+
+        const fakerData = Array(5)
+            .fill(0)
+            .map((item, index) => {
+                return {
+                    image: images[index]
+                };
+            });
         return (
             <div className="trust-us-rect">
                 <div className="max-width-1090">
@@ -13,6 +57,18 @@ class TrustUs extends React.Component {
                             <div>НАМ ДОВЕРЯЮТ</div>
                             <div>ПАРТНЕРЫ</div>
                         </div>
+                    </div>
+                    <div className="carousel">
+                        <Carousel
+                            responsive={responsive}
+                            infinite
+                            customRightArrow={<CustomRightArrow />}
+                            customLeftArrow={<CustomLeftArrow />}
+                        >
+                            {fakerData.map(card => {
+                                return <img className="img-style" src={card.image} alt="img" />;
+                            })}
+                        </Carousel>
                     </div>
                 </div>
             </div>
