14c56ae51b60370ae6583ee74e6ca9301c4a5005
Anastasiya Karpenka <knesty@yandex.by>
2022-09-16 15:42:35 +0300

    smth old

--- frontend/src/components/product-card/ProductCard.js
+++ frontend/src/components/product-card/ProductCard.js
@@ -9,18 +9,12 @@ export default function ProductCard(props) {
             <div className="product-card-header font-s-14 ml-3 mb-2">{props.name}</div>
             <img src={props.image} alt="img" />
             <div className="button-position">
-                <Link
-                    to={{
-                        pathname: `/portfolio/${props.link}`,
-                        state: { category: 'props.link' }
-                    }}
-                >
+                <Link to={`/portfolio/${props.link}`}>
                     <button type="button" className="btn btn-navy">
                         СМОТРЕТЬ
                     </button>
                 </Link>
             </div>
-            <div>{props.link}</div>
         </div>
     );
 }
