8b63fc9739d9f966e4a34ee35537a40b02122683
Anastasiya Karpenka <knesty@yandex.by>
2022-10-12 14:03:04 +0300

    added validation

--- frontend/src/components/modals/call/CallModal.js
+++ frontend/src/components/modals/call/CallModal.js
@@ -1,19 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import InputMask from 'react-input-mask';
 import { askForRecalling } from '../../../redux/requestSlice';
 import { hideCallModal } from '../../../redux/modalSlice';
 import Spinner from '../../spinner/Spinner';
 import './CallModal.scss';
 
 export default function CallModal() {
-    const [username, setUsername] = useState('');
+    const [userName, setUsername] = useState('');
+    const [userNameDirty, setUserNameDirty] = useState(false);
+    const [userNameError, setUserNameError] = useState('Заполните, пожалуйста, обязательное поле');
     const [telephone, setTelephone] = useState('');
+    const [telephoneDirty, setTelephoneDirty] = useState(false);
     const [message, setMessage] = useState('');
+    const [formValid, setFormValid] = useState(false);
+
     const state = useSelector((state) => state);
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        if (userNameError || telephoneDirty) {
+            setFormValid(false);
+        } else {
+            setFormValid(true);
+        }
+    }, [userNameError,telephoneDirty]);
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'userName':
+                setUserNameDirty(true);
+                break;
+            case 'telephone':
+                let val = e.target.value.split('');
+                if (val.length == 0) {
+                    setTelephoneDirty(true)
+                } else {
+                    val.some(el => el == '_') ? setTelephoneDirty(true) : setTelephoneDirty(false);
+                }
+                break;
+        }
+    }
+
     const handleUsernameChange = event => {
         setUsername(event.target.value);
+        if (event.target.value == '') {
+            setUserNameError('Заполните, пожалуйста, обязательное поле');
+        } else if (!/^[a-zA-ZЁёА-я\-]+$/.test(event.target.value)) {
+            setUserNameError('Имя некорректно');
+        } else {
+            setUserNameError('');
+        }
     };
 
     const handleTelephoneChange = event => {
@@ -26,8 +63,8 @@ export default function CallModal() {
 
     const handleSubmit = event => {
         event && event.preventDefault();
-        if (!!username && !!telephone) {
-            dispatch(askForRecalling({ username, telephone, message}));
+        if (!!userName && !!telephone) {
+            dispatch(askForRecalling({ userName, telephone, message}));
         }
     };
 
@@ -43,41 +80,56 @@ export default function CallModal() {
             <form onSubmit={handleSubmit}>
                 <fieldset disabled={state.request.sending}>
                     <div className="fields">
-                        <label className="input">
-                            <input
-                                className="input__field"
-                                type="text"
-                                placeholder=" "
-                                value={username}
-                                onChange={handleUsernameChange}
-                                required
-                            />
-                            <span className="input__label">Ваше имя *</span>
-                        </label>
                         <div className="field">
                             <label className="input">
-                                <textarea
+                                <input
                                     className="input__field"
+                                    type="text"
                                     placeholder=" "
-                                    value={message}
-                                    onChange={handleMessageChange}
+                                    value={userName}
+                                    onChange={handleUsernameChange}
+                                    name = "userName"
+                                    onBlur={blurHandler}
+                                    required
                                 />
-                                <span className="input__label">Ваше сообщение</span>
+                                <span className="input__label">Ваше имя *</span>
                             </label>
+                            {(userNameDirty && userNameError) && <p className="errorText"> {userNameError}</p>}
                         </div>
-                        <label className="input">
-                            <input
-                                className="input__field"
-                                type="text"
-                                placeholder=" "
-                                value={telephone}
-                                onChange={handleTelephoneChange}
-                                required
-                            />
-                            <span className="input__label">Ваш телефон *</span>
-                        </label>
+
+                        <div className="field">
+                            <label className="input">
+                                <InputMask 
+                                        mask="+375\(99)-999-99-99"   
+                                        maskChar="_" 
+                                        className="input__field"
+                                        type="text"
+                                        placeholder=" "
+                                        value={telephone}
+                                        onChange={handleTelephoneChange}
+                                        name = "telephone"
+                                        onBlur={blurHandler}
+                                        required
+                                />
+                                <span className="input__label">Ваш телефон *</span>
+                            </label>
+                            {(telephoneDirty) && <p className="errorText">Заполните, пожалуйста, обязательное поле</p>}
+                        </div>
+
+                            <div className="field">
+                                <label className="input">
+                                    <textarea
+                                        className="input__field"
+                                        placeholder=" "
+                                        value={message}
+                                        onChange={handleMessageChange}
+                                    />
+                                    <span className="input__label">Ваше сообщение</span>
+                                </label>
+                            </div>
+                        
                     </div>
-                    <button type="submit" className="btn btn-navy">
+                    <button type="submit" disabled={!formValid} className="btn btn-navy">
                         {state.request.sending ? <Spinner /> : 'ОТПРАВИТЬ'}
                     </button>
                 </fieldset>
--- frontend/src/components/modals/send/SendModal.js
+++ frontend/src/components/modals/send/SendModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { sendDesign } from '../../../redux/requestSlice';
 import { hideSendModal } from '../../../redux/modalSlice';
@@ -9,18 +9,57 @@ import 'react-dropzone-uploader/dist/styles.css';
 import { getDroppedOrSelectedFiles } from 'html5-file-selector';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFolderOpen } from "@fortawesome/free-regular-svg-icons";
+import InputMask from 'react-input-mask';
 
 
 export default function SendModal() {
-    const [username, setUsername] = useState('');
+
+    const [userName, setUsername] = useState('');
+    const [userNameDirty, setUserNameDirty] = useState(false);
+    const [userNameError, setUserNameError] = useState('Заполните, пожалуйста, обязательное поле');
     const [telephone, setTelephone] = useState('');
+    const [telephoneDirty, setTelephoneDirty] = useState(false);
     const [message, setMessage] = useState('');
+    const [formValid, setFormValid] = useState(false);
+
     const [files, setFile] = useState([]);
     const state = useSelector((state) => state);
     const dispatch = useDispatch();
+    
+    useEffect(() => {
+        if (userNameError || telephoneDirty) {
+            setFormValid(false);
+        } else {
+            setFormValid(true);
+        }
+    }, [userNameError,telephoneDirty]);
+
+    
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'userName':
+                setUserNameDirty(true);
+                break;
+            case 'telephone':
+                let val = e.target.value.split('');
+                if (val.length == 0) {
+                    setTelephoneDirty(true)
+                } else {
+                    val.some(el => el == '_') ? setTelephoneDirty(true) : setTelephoneDirty(false);
+                }
+                break;
+        }
+    }
 
     const handleUsernameChange = event => {
         setUsername(event.target.value);
+        if (event.target.value == '') {
+            setUserNameError('Заполните, пожалуйста, обязательное поле');
+        } else if (!/^[a-zA-ZЁёА-я\-]+$/.test(event.target.value)) {
+            setUserNameError('Имя некорректно');
+        } else {
+            setUserNameError('');
+        }
     };
 
     const handleTelephoneChange = event => {
@@ -33,8 +72,8 @@ export default function SendModal() {
 
     const handleSubmit = event => {
         event && event.preventDefault();
-        if (!!username && !!telephone && !!files) {
-            dispatch(sendDesign({ username, telephone, message, files }));
+        if (!!userName && !!telephone && !!files) {
+            dispatch(sendDesign({ userName, telephone, message, files }));
         }
     };
 
@@ -105,39 +144,54 @@ export default function SendModal() {
                 <form onSubmit={handleSubmit}>
                     <fieldset disabled={state.request.sending}>
                         <div className="fields">
-                            <label className="input">
-                                <input
-                                    className="input__field"
-                                    type="text"
-                                    placeholder=" "
-                                    value={username}
-                                    onChange={handleUsernameChange}
-                                    required
-                                />
-                                <span className="input__label">Ваше имя *</span>
-                            </label>
                             <div className="field">
                                 <label className="input">
-                                    <textarea
+                                    <input
                                         className="input__field"
+                                        type="text"
                                         placeholder=" "
-                                        value={message}
-                                        onChange={handleMessageChange}
+                                        value={userName}
+                                        onChange={handleUsernameChange}
+                                        name = "userName"
+                                        onBlur={blurHandler}
+                                        required
                                     />
-                                    <span className="input__label">Ваше сообщение</span>
+                                    <span className="input__label">Ваше имя *</span>
                                 </label>
+                                {(userNameDirty && userNameError) && <p className="errorText"> {userNameError}</p>}
                             </div>
-                            <label className="input">
-                                <input
-                                    className="input__field"
-                                    type="text"
-                                    placeholder=" "
-                                    value={telephone}
-                                    onChange={handleTelephoneChange}
-                                    required
-                                />
-                                <span className="input__label">Ваш телефон *</span>
-                            </label>
+
+                            <div className="field">
+                                <label className="input">
+                                    <InputMask 
+                                            mask="+375\(99)-999-99-99"   
+                                            maskChar="_" 
+                                            className="input__field"
+                                            type="text"
+                                            placeholder=" "
+                                            value={telephone}
+                                            onChange={handleTelephoneChange}
+                                            name = "telephone"
+                                            onBlur={blurHandler}
+                                            required
+                                    />
+                                    <span className="input__label">Ваш телефон *</span>
+                                </label>
+                                {(telephoneDirty) && <p className="errorText">Заполните, пожалуйста, обязательное поле</p>}
+                            </div>
+
+                                <div className="field">
+                                    <label className="input">
+                                        <textarea
+                                            className="input__field"
+                                            placeholder=" "
+                                            value={message}
+                                            onChange={handleMessageChange}
+                                        />
+                                        <span className="input__label">Ваше сообщение</span>
+                                    </label>
+                                </div>
+                            
                         </div>
                         <div className="upload-button">
                             <Dropzone
@@ -164,15 +218,16 @@ export default function SendModal() {
                                 }}       
                             />
                         </div>
-                        <button type="submit" className="btn btn-navy">
+                        <button type="submit" disabled={!formValid} className="btn btn-navy">
                             {state.request.sending ? <Spinner /> : 'ОТПРАВИТЬ'}
                         </button>
+                        <p className="footer-modal font-s-12 text-center">
+                            Нажимая на кнопку, вы даете согласие на обработку своих персональных данных
+                        </p>
                     </fieldset>
                 </form>
             </div>
-            <p className="footer-modal font-s-12 text-center">
-                Нажимая на кнопку, вы даете согласие на обработку своих персональных данных
-            </p>
+            
         </div>
     );
 }
