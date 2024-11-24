2f13ed167275b94e010a74ac6c04d703cfa22418
AnKlim <andrewklimenkov@gmail.com>
2024-11-24 17:52:03 +0300

    version upgrage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,18 +11,27 @@ import { store } from './redux';
 import './index.scss';
 import UpArrow from './components/up-arrow/UpArrow';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <UpArrow />
-        <Router history={createBrowserHistory}>
-            <Switch>
-                <Redirect exact from="/" to="/main" />
-                <Route path="/main" component={Main} />
-                <Route path="/contacts" component={Contacts} />
-                <Route path="/portfolio/:category" component={Portfolio} />
-                <Route path="/about-us" component={AboutUs} />
-            </Switch>
-        </Router>
-    </Provider>,
-    document.getElementById('root')
+// const router = createBrowserRouter([
+//   { path: "/", element: <Main /> },
+//   { path: "/contacts", element: <Contacts /> },
+//   { path: "/portfolio/:category", element: <Portfolio /> },
+//   { path: "/about-us", element: <AboutUs /> },
+// ]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  //   <ReouterProvider router={router} />
+  // </React.StrictMode>
+  <Provider store={store}>
+      <UpArrow />
+      <Router history={createBrowserHistory}>
+          <Switch>
+              <Redirect exact from="/" to="/main" />
+              <Route path="/main" component={Main} />
+              <Route path="/contacts" component={Contacts} />
+              <Route path="/portfolio/:category" component={Portfolio} />
+              <Route path="/about-us" component={AboutUs} />
+          </Switch>
+      </Router>
+  </Provider>
 );
