7417a27002d189be3705cd696b5e64b1b698f46f
Anastasiya <knesty@yandex.by>
2025-01-08 23:42:54 +0300

    fix(gallery): исправлено формирование галереи | mtl-3

--- frontend/src/components/gallery/Gallery.js
+++ frontend/src/components/gallery/Gallery.js
@@ -2,24 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import GridGallery from './grid-gallery/GridGallery';
-import IMAGES from '../../constants/imagesStore';
+import {IMAGES, CATEGORIES} from '../../constants/imagesStore';
+import { HashLink } from 'react-router-hash-link';
+
 import './Gallery.scss';
 
 export default function Gallery() {
-    const [typeOfCategory, setTypeOfCategory] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
 
-    useEffect(() => {
-        if (typeOfCategory === '') {
-            setTypeOfCategory(location.pathname.split('/')[2]);
-            navigate(`/portfolio/${location.pathname.split('/')[2]}`);
-        } else {
-            navigate(`/portfolio/${typeOfCategory}`);
-        }
-    }, [typeOfCategory]);
+    const [typeOfCategory, setTypeOfCategory] = useState(location.pathname.split('/')[2]);
+    const images = IMAGES[typeOfCategory];
 
-    const images = IMAGES.filter(photo => photo.category === typeOfCategory);
+    useEffect(() => {
+        navigate(`/portfolio/${typeOfCategory}`);
+    }, [typeOfCategory]);
 
     return (
         <div className="gallery-rect">
@@ -36,86 +33,34 @@ export default function Gallery() {
                     </p>
                     <div className="gallery-menu">
                         <table>
-                            <tr
-                                className={typeOfCategory === 'FENCE' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('FENCE')}
-                            >
-                                <td>Заборы</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'GATE' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('GATE')}
-                            >
-                                <td>Ворота и калитки</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'RAILINGS' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('RAILINGS')}
-                            >
-                                <td>Перила</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'GRILLES' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('GRILLES')}
-                            >
-                                <td>Решетки</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'GREENHOUSES' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('GREENHOUSES')}
-                            >
-                                <td>Теплицы</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'VISOR' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('VISOR')}
-                            >
-                                <td>Козырьки</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'CANOPIES' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('CANOPIES')}
-                            >
-                                <td>Навесы</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'STAIRS' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('STAIRS')}
-                            >
-                                <td>Лестницы</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'SHELVES' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('SHELVES')}
-                            >
-                                <td>Стеллажи</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'BILLBOARDS' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('BILLBOARDS')}
-                            >
-                                <td>Рекламные билборды</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'FRAMES' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('FRAMES')}
-                            >
-                                <td>Металлические каркасы</td>
-                            </tr>
-                            <tr
-                                className={typeOfCategory === 'OTHER' ? 'nav-active' : ''}
-                                onClick={() => setTypeOfCategory('OTHER')}
-                            >
-                                <td>Другое</td>
-                            </tr>
+                            {Object.entries(CATEGORIES).map(([key, value]) => {
+                                return (
+                                    <tr
+                                        key={key}
+                                        className={typeOfCategory === key ? 'nav-active' : ''}
+                                        onClick={() => setTypeOfCategory(key)}
+                                    >
+                                        <td>{value.rusName}</td>
+                                    </tr>
+                                )
+                            })}
                         </table>
                         <div className="pictures">
-                            <p className="pic-title">Заборы</p>
+                            <p className="pic-title">{CATEGORIES[typeOfCategory].rusName}</p>
+                            <p className="pic-more-about">
+                                <HashLink smooth to="#about-anchor">(Узнать больше)</HashLink>
+                            </p>
                             <p className="pic-subtitle">{images.length} фото</p>
                             <GridGallery images={images}/>
                         </div>
                     </div>
                 </div>
+
+                <div className='about-container'>
+                    <span id='about-anchor'></span>
+                    <p className='about-category'>{CATEGORIES[typeOfCategory].about}</p>
+                </div>
+                
             </div>
         </div>
     );
--- frontend/src/constants/imagesStore.js
+++ frontend/src/constants/imagesStore.js
@@ -1,68 +1,142 @@
-const IMAGES = [
-    {
-        src: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
-        thumbnail: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg',
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        caption: 'After Rain (Jeshu John - designerspics.com)',
-        category: 'FENCE'
-    },
-    {
-        src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
-        thumbnail: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        caption: '111Boats (Jeshu John - designerspics.com)',
-        category: 'GATE'
-    },
-    {
-        src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-        thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        caption: 'Boats (Jeshu John - designerspics.com)',
-        category: 'FENCE'
-    },
-    {
-        src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-        thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        caption: 'Boats (Jeshu John - designerspics.com)',
-        category: 'FENCE'
-    },
-    {
-        src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-        thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        caption: 'Boats (Jeshu John - designerspics.com)',
-        category: 'FENCE'
-    },
-    {
-        src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-        thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        caption: 'Boats (Jeshu John - designerspics.com)',
-        category: 'FENCE'
-    },
-    {
-        src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-        thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        caption: 'Boats (Jeshu John - designerspics.com)',
-        category: 'FENCE'
-    },
-    {
-        src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-        thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        caption: 'Boats (Jeshu John - designerspics.com)',
-        category: 'FENCE'
-    }
-];
+export const CATEGORIES = {
+    FENCE: {
+        rusName: 'Заборы',
+        engName: 'FENCE',
+        //ToDo: придумать что-то нормальное, может быть отдельные константы 
+        about: `Скрыть и оградить свои постройки вы сможете при помощи забора или ограждения. В качестве материалов для ограждения нами используется сетка рабица или металлопрофиль.
+        _____________________________________
+       Сетка рабица, это удачный вариант ограждения для участков с большой растительностью. Она пропускает солнечные лучи, влагу и воздух, что полезно для садовых растений. Сетка может быть из черной или оцинкованной проволоки, последняя не боится коррозии, благодаря защитному покрытию из цинка. Она отличается более привлекательнымвидом и не нуждается в дополнительном покрытии. В качестве опор используются профильные трубы.
 
-export default IMAGES;
+       Цена за 2 м.п. от 65 руб.
+
+       Срок изготовления от 10 рабочих дней.
+       _____________________________________
+
+       Современный материал для забора- металлопрофиль. Такой материал, как профнастил можно смело назвать одним из самых качественных материалов для изготовления заборов.
+
+       Преимущества заборов из профлиста:
+
+        - прочность (жесткость);
+
+        - устойчивость к коррозии;
+
+        - эстетическая привлекательность;
+
+        - нетребовательность к уходу;
+
+        - длительный срок эксплуатации;
+
+            Основные материалы, которые используются для изготовления забора из металлопрофиля:
+
+        - Профнастил (существует огромный выбор окраса и видов профиля
+
+        листов и их размеры);
+
+        - Опорные стойки (столбы квадратного сечения)
+
+        - Поперечные лаги (квадратные трубы)
+
+        - Пластина держатель (кронштейн) для профнастила
+
+        - Заглушки для столбов (опор)
+
+        - Метизы и др.
+
+        Цена за 2 м.п. от 190 руб.
+
+        Срок изготовления от 10 рабочих дней.`
+    },
+    GATE: {
+        rusName: 'Ворота и калитки',
+        engName: 'GATE',
+        about: '',
+    },
+    RAILINGS: {
+        rusName: 'Перила',
+        engName: 'RAILINGS',
+        about: '',
+    },
+    GRILLES: {
+        rusName: 'Решетки',
+        engName: 'GRILLES',
+        about: '',
+    },
+    GREENHOUSES: {
+        rusName: 'Теплицы',
+        engName: 'GREENHOUSES',
+        about: '',
+    },
+    VISOR: {
+        rusName: 'Козырьки',
+        engName: 'VISOR',
+        about: '',
+    },
+    CANOPIES: {
+        rusName: 'Навесы',
+        engName: 'CANOPIES',
+        about: '',
+    },
+    STAIRS: {
+        rusName: 'Лестницы',
+        engName: 'STAIRS',
+        about: '',
+    },
+    SHELVES: {
+        rusName: 'Стеллажи',
+        engName: 'SHELVES',
+        about: '',
+    },
+    BILLBOARDS: {
+        rusName: 'Рекламные билборды',
+        engName: 'BILLBOARDS',
+        about: '',
+    },
+    FRAMES: {
+        rusName: 'Металлические каркасы',
+        engName: 'FRAMES',
+        about: '',
+    },
+    OTHER: {
+        rusName: 'Другое',
+        engName: 'OTHER',
+        about: '',
+    },
+};
+
+export const IMAGES = {
+    [CATEGORIES.FENCE.engName]: [
+        {
+            src: 'src/assets/images/portfolio/fences/1.jpg',
+            caption: 'Забор из блабла. Цена: от 1234$',
+        },
+        {
+            src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
+            caption: 'Забор из блабла. Цена: от 1234$',
+        },
+        {
+            src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
+            caption: 'Забор из блабла. Цена: от 1234$',
+        },
+        {
+            src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
+            caption: 'Забор из блабла. Цена: от 1234$',
+        },
+    ],
+    [CATEGORIES.GATE.engName]: [
+        {
+            src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
+            caption: '111Boats (Jeshu John - designerspics.com)',
+        },
+    ],
+    [CATEGORIES.RAILINGS.engName]: [],
+    [CATEGORIES.GRILLES.engName]: [],
+    [CATEGORIES.GREENHOUSES.engName]: [],
+    [CATEGORIES.VISOR.engName]: [],
+    [CATEGORIES.CANOPIES.engName]: [],
+    [CATEGORIES.STAIRS.engName]: [],
+    [CATEGORIES.SHELVES.engName]: [],
+    [CATEGORIES.BILLBOARDS.engName]: [],
+    [CATEGORIES.FRAMES.engName]: [],
+    [CATEGORIES.OTHER.engName]: []
+};
+
